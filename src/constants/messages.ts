67a8d2085ea6fc5fe023a2ec87bf09a2a86f4676
Stefano Palomino <stefanop21@outlook.es>
2024-11-18 23:37:20 -0500

    refactor(constants): added how it works messages

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -11,24 +11,51 @@ export const whyUs = [
     title: 'Escoge a tu cuidador',
     description:
       'La mejor opción para tu mascota. Escoge a tu cuidador y disfruta de la tranquilidad de saber que tu mascota está en buenas manos',
-    image: 'why-us-1.jpg',
+    image: '/why-us/1.jpg',
   },
   {
     title: 'Atención personalizada',
     description:
       'Haz que tu mascota se sienta como en casa. Ofrecemos atención personalizada para que tu mascota se sienta cómoda y feliz.',
-    image: 'why-us-2.jpg',
+    image: '/why-us/2.jpg',
   },
   {
     title: 'Cuidado especializado',
     description:
       'Nos preocupamos por el bienestar de tu mascota. Nuestros cuidadores están capacitados para brindar la mejor atención a tus amigos peludos.',
-    image: 'why-us-3.jpg',
+    image: '/why-us/3.jpg',
   },
   {
     title: 'Más tiempo para ti',
     description:
       'Deja de preocuparte por el cuidado de tu mascota. Nosotros nos encargamos de todo para que tú puedas disfrutar de tu tiempo libre.',
-    image: 'why-us-4.jpg',
+    image: '/why-us/4.jpg',
+  },
+];
+
+export const howItWorks = [
+  {
+    title: 'Busca y elige',
+    description:
+      'Busca la familia perfecta para cuidar a tu mascota. Puedes realizar vistas previas para que conoscas el lugar y la familia que cuidará a tu mascota por ti.',
+    image: '/how-it-works/1.png',
+  },
+  {
+    title: 'Reserva',
+    description:
+      'Cuando haces una reserva, la propiedad queda bloqueada hasta que el propietario responda a tu solicitud con un máximo de 24 horas.',
+    image: '/how-it-works/2.png',
+  },
+  {
+    title: 'Confirmación',
+    description:
+      'Tan pronto el cuidador acepte la reserva, cargaremos el importe corespondiente en tu metodo de pago.',
+    image: '/how-it-works/3.png',
+  },
+  {
+    title: 'Fecha de entrada',
+    description:
+      'El siguiente paso es dejar a tu mascota con la familia elegida.',
+    image: '/how-it-works/4.png',
   },
 ];
