dfd29d2f4653cfba0854ce9e1637ae4bacf0d8ed
Stefano Palomino <stefanop21@outlook.es>
2024-11-18 21:52:47 -0500

    feat(constants): added why us messages

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -5,3 +5,30 @@ export const wspMessageService =
   '¡Hola! Estoy explorando la web y me encontré con tu sitio. Estoy interesado en el servicio ';
 
 export const wspNumber = '51987052642';
+
+export const whyUs = [
+  {
+    title: 'Escoge a tu cuidador',
+    description:
+      'La mejor opción para tu mascota. Escoge a tu cuidador y disfruta de la tranquilidad de saber que tu mascota está en buenas manos',
+    image: 'why-us-1.jpg',
+  },
+  {
+    title: 'Atención personalizada',
+    description:
+      'Haz que tu mascota se sienta como en casa. Ofrecemos atención personalizada para que tu mascota se sienta cómoda y feliz.',
+    image: 'why-us-2.jpg',
+  },
+  {
+    title: 'Cuidado especializado',
+    description:
+      'Nos preocupamos por el bienestar de tu mascota. Nuestros cuidadores están capacitados para brindar la mejor atención a tus amigos peludos.',
+    image: 'why-us-3.jpg',
+  },
+  {
+    title: 'Más tiempo para ti',
+    description:
+      'Deja de preocuparte por el cuidado de tu mascota. Nosotros nos encargamos de todo para que tú puedas disfrutar de tu tiempo libre.',
+    image: 'why-us-4.jpg',
+  },
+];
