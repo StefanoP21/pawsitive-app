76811c80015a237a69a5ff1f0b51c9fdacdc36e2
Stefano Palomino <stefanop21@outlook.es>
2024-11-19 17:51:33 -0500

    config(app): added remotePatterns for images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'galery-doghouser.s3-us-west-2.amazonaws.com',
+      },
+    ],
+  },
   env: {
     PUBLIC_ASSETS_URL: process.env.PUBLIC_ASSETS_URL,
   },
