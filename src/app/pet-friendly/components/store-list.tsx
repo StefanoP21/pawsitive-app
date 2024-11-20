06c5bad532a85f4974b4413f80f92bfd70deaa68
Stefano Palomino <stefanop21@outlook.es>
2024-11-20 16:49:49 -0500

    refactor(pet-friendly): added link page in card

--- src/app/pet-friendly/components/store-list.tsx
+++ src/app/pet-friendly/components/store-list.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { PetFriendly } from '@/db/pet-friendly/model';
+import Link from 'next/link';
 
 interface StoreListProps {
   stores: PetFriendly[];
@@ -29,16 +30,19 @@ export default function StoreList({ stores }: StoreListProps) {
         {stores.map((item, index) => (
           <article
             key={index}
-            className="flex flex-col gap-2 w-full sm:w-44 lg:w-48 h-auto hover:bg-primary/10 p-2 rounded-2xl"
+            className="flex flex-col gap-4 w-full sm:w-44 lg:w-48 h-auto hover:bg-primary/10 p-2 rounded-2xl"
           >
-            <Image
-              src={`${process.env.PUBLIC_ASSETS_URL}/${item.image_card}`}
-              alt={item.title}
-              width={200}
-              height={200}
-              className="object-cover w-full h-60 rounded-xl shadow-md"
-            />
-            <div className="flex flex-col gap-1">
+            <Link href={item.link_page} target="_blank">
+              <Image
+                src={`${process.env.PUBLIC_ASSETS_URL}/${item.image_card}`}
+                alt={item.title}
+                width={200}
+                height={200}
+                className="object-cover w-full h-60 rounded-xl shadow-md"
+                loading="lazy"
+              />
+            </Link>
+            <div className="flex flex-col gap-1 cursor-default">
               <h3 className="text-xl font-semibold text-gray-700 truncate">
                 {item.title}
               </h3>
