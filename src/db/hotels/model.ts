ca84d2758634cf95c62126f1556f4932d7b4eb80
Stefano Palomino <stefanop21@outlook.es>
2024-11-20 16:48:30 -0500

    feat(db): added hotels data en model

---
+++ src/db/hotels/model.ts
@@ -0,0 +1,13 @@
+export interface Hotel {
+  id: number;
+  image: string;
+  name: string;
+  phoneContact: string;
+  address: string;
+  email: string;
+  rating: number;
+  ratingCount: number;
+  minPrice: number;
+  maxPrice: number;
+  link_page: string;
+}
