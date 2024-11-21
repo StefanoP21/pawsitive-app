426d9618d7d45d882c77777b6215c162466fb3e2
Stefano Palomino <stefanop21@outlook.es>
2024-11-20 19:37:02 -0500

    refactor(db): changed hotel model and data

--- src/db/hotels/model.ts
+++ src/db/hotels/model.ts
@@ -2,12 +2,22 @@ export interface Hotel {
   id: number;
   image: string;
   name: string;
+  description: string;
+  service_type: string;
+  schedule: string;
   phoneContact: string;
   address: string;
   email: string;
-  rating: number;
-  ratingCount: number;
   minPrice: number;
   maxPrice: number;
   link_page: string;
+  location: Location;
+}
+
+export interface Location {
+  country: string;
+  department: string;
+  province: string;
+  district: string;
+  link_map: string | null;
 }
