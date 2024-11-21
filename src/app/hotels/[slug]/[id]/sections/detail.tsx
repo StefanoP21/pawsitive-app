1b92657a263bbbf396f5c546a8e21d6be7315111
Stefano Palomino <stefanop21@outlook.es>
2024-11-20 21:10:43 -0500

    refactor(hotels): changed hotel labels in detail section

--- src/app/hotels/[slug]/[id]/sections/detail.tsx
+++ src/app/hotels/[slug]/[id]/sections/detail.tsx
@@ -71,10 +71,12 @@ export default function Detail() {
 
           <article className="grid gap-2 border border-chart-5/50 rounded-lg p-5">
             <h2 className="text-chart-5 text-2xl font-semibold">Contácto</h2>
-            <div className="text-gray-600">
-              <h3 className="text-lg font-semibold">Correo</h3>
-              <p>{hotel?.email}</p>
-            </div>
+            {hotel?.email && (
+              <div className="text-gray-600">
+                <h3 className="text-lg font-semibold">Correo</h3>
+                <p>{hotel?.email}</p>
+              </div>
+            )}
             <div className="text-gray-600">
               <h3 className="text-lg font-semibold">Teléfono</h3>
               <p>{hotel?.phoneContact}</p>
@@ -93,26 +95,28 @@ export default function Detail() {
 
           <article className="grid col-span-1 sm:col-span-2 gap-2 border border-chart-5/50 rounded-lg p-5">
             <h2 className="text-chart-5 text-2xl font-semibold">Ubicación</h2>
-            <div className="text-gray-600">
-              <h3 className="text-lg font-semibold">Dirección</h3>
-              <p>{hotel?.address}</p>
-              <p>
-                {hotel?.location?.department} - {hotel?.location?.province} -{' '}
-                {hotel?.location?.district}
-              </p>
-            </div>
-            {hotel?.location?.link_map && (
-              <div className="text-gray-600">
-                <h3 className="text-lg font-semibold">Mapa</h3>
-                <a
-                  href={hotel?.location.link_map}
-                  target="_blank"
-                  className="text-chart-5"
-                >
-                  <FaMapMarkedAlt size={30} />
-                </a>
+            {hotel?.address?.map((address, index) => (
+              <div key={index} className="flex gap-4">
+                <div className="text-gray-600">
+                  <h3 className="text-lg font-semibold">Dirección</h3>
+                  <p>{address?.detail}</p>
+                  <p>
+                    {hotel?.location?.department} - {hotel?.location?.province}{' '}
+                    - {hotel?.location?.district}
+                  </p>
+                </div>
+                <div className="text-gray-600">
+                  <h3 className="text-lg font-semibold">Mapa</h3>
+                  <a
+                    href={address?.link_map ?? undefined}
+                    target="_blank"
+                    className="text-chart-5"
+                  >
+                    <FaMapMarkedAlt size={30} />
+                  </a>
+                </div>
               </div>
-            )}
+            )) ?? 'No disponible'}
           </article>
         </div>
       </section>
